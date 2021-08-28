1c585e3a091432c40d622f3ceea9a613ad8aa223
Branley Minaya <bminaya2@gmail.com>
2021-08-28 18:24:16 -0400

    commit response

--- server_practice/server.js
+++ server_practice/server.js
@@ -1,9 +1,26 @@
-const express = required("express");
+const express = require("express");
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get()
+// CRUD = create, read, update, delete
+// this is a read(just reading the info being passed)
+app.get("/api/welcome", (request, response) => {
+  console.log("This is welcome API route");
+  console.log(request);
+  // you must respond! wont work without it
+  response.send({message: "Welcome to our API"});
+})
+
+app.post("/api/welcome", (request, response) => {
+  response.json({
+    message: "The API is cool!", 
+    requestBody: request.body
+  })
+  console.log(request.body);
+})
 
 
 
-app. listen(8000, () => console.lof("You have successfully connected to port 8000"));
+app. listen(8000, () => console.log("You have successfully connected to port 8000"));
