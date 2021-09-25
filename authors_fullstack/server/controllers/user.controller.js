36d3e56011d3ce8f05b792cf9f1e62579d403d5e
Branley Minaya <bminaya23@gmail.com>
2021-09-25 09:29:59 -0400

    add dependencies

---
+++ authors_fullstack/server/controllers/user.controller.js
@@ -0,0 +1,3 @@
+const User = require('../models/user.model');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
---
+++ authors_fullstack/server/models/user.model.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Username is required"]
+  },
+  email:{
+    type: String,
+    required: [ture, "Email is required"]
+  },
+  password:{
+    type: String,
+    required: [true, "password is required"],
+    minlength: [8, "password requires atleast 8 characters"]
+  },
+}, {timestamps: true})
+
+UserSchema.virtual('confirmPassword')
+.get(()=> this._confirmPassword)
+.set((value)=> this._confirmPassword = value)
+
+UserSchema.pre("validate", function(next){
+  console.log('in validate');
+  if(this.password !== confirmPassword){
+  this.invalidate("confirmPassword", "Password must match")
+  console.log("didn't match");
+  }
+  next();
+})
+
+UserSchema.pre("save",function(next){
+  console.log("in pre save");
+  bcrypt.hash(this.password, 10)
+  .then((hashPassword)=>{
+    console.log("in hash");
+    this.password = hashPassword;
+    next();
+  })
+});
+
+
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
