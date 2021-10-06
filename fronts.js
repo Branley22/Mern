28a5eeeb68462a02f05302d0f1fdefea72f13da6
Branley Minaya <bminaya23@gmail.com>
2021-10-06 17:22:24 -0400

    fronts SLL

---
+++ fronts.js
@@ -0,0 +1,8 @@
+//Fronts
+// Write a method that accepts a value and create a new node, assign it to the list head, and return a pointer to the new head node.
+
+class SLL {
+  addFront(val){
+    
+  }
+}
